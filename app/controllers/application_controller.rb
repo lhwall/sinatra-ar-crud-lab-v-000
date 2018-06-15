7d35f9f6affd4790fa48b4b95a6c863dcaca5dbd
Lucy Wall <lhwall@gmail.com>
2018-06-15 18:40:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
+    @post.save
   erb :show
   end
 
